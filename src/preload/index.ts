f17c8b2ec3362aa73137182487670ab863e922cb
Eugene Asare <eugenemindset@gmail.com>
2025-03-06 23:06:28 -0500

    whole switcherooo

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,22 +1,24 @@
-import { contextBridge } from 'electron';
-import { electronAPI } from '@electron-toolkit/preload';
+import { ipcRenderer, contextBridge } from 'electron'
 
-// Custom APIs for renderer
-const api = {};
+// --------- Expose some API to the Renderer process ---------
+contextBridge.exposeInMainWorld('ipcRenderer', {
+  on(...args: Parameters<typeof ipcRenderer.on>) {
+    const [channel, listener] = args
+    return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
+  },
+  off(...args: Parameters<typeof ipcRenderer.off>) {
+    const [channel, ...omit] = args
+    return ipcRenderer.off(channel, ...omit)
+  },
+  send(...args: Parameters<typeof ipcRenderer.send>) {
+    const [channel, ...omit] = args
+    return ipcRenderer.send(channel, ...omit)
+  },
+  invoke(...args: Parameters<typeof ipcRenderer.invoke>) {
+    const [channel, ...omit] = args
+    return ipcRenderer.invoke(channel, ...omit)
+  },
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
-if (process.contextIsolated) {
-  try {
-    contextBridge.exposeInMainWorld('electron', electronAPI);
-    contextBridge.exposeInMainWorld('api', api);
-  } catch (error) {
-    console.error(error);
-  }
-} else {
-  // @ts-ignore (define in dts)
-  window.electron = electronAPI;
-  // @ts-ignore (define in dts)
-  window.api = api;
-}
+  // You can expose other APTs you need here.
+  // ...
+})
---
+++ vite.config.ts
@@ -0,0 +1,76 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+import pkg from "./package.json";
+import electron from "vite-plugin-electron/simple";
+import { rmSync } from "node:fs";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  rmSync("out", { recursive: true, force: true });
+
+  const isServe = command === "serve";
+  const isBuild = command === "build";
+  const sourcemap = isServe || !!process.env.VSCODE_DEBUG;
+
+  return {
+    plugins: [
+      electron({
+        main: {
+          // Shortcut of `build.lib.entry`.
+          entry: "src/main/index.ts",
+          onstart(args) {
+            if (process.env.VSCODE_DEBUG) {
+              console.log(
+                /* For `.vscode/.debug.script.mjs` */ "[startup] Electron App"
+              );
+            } else {
+              args.startup();
+            }
+          },
+          vite: {
+            build: {
+              sourcemap,
+              minify: isBuild,
+              outDir: "out/main",
+            },
+          },
+        },
+        preload: {
+          // Shortcut of `build.rollupOptions.input`.
+          // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
+          input: path.join(__dirname, "src/preload/index.ts"),
+          vite: {
+            build: {
+              sourcemap: sourcemap ? true : undefined, // #332
+              minify: isBuild,
+              outDir: "out/preload",
+            },
+          },
+        },
+        // Ployfill the Electron and Node.js API for Renderer process.
+        // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
+        // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+        // renderer: process.env.NODE_ENV === 'test'
+        //   // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
+        //   ? undefined
+        //   : {},
+        renderer: {},
+      }),
+    ],
+    server:
+      process.env.VSCODE_DEBUG &&
+      (() => {
+        const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL);
+        return {
+          host: url.hostname,
+          port: +url.port,
+        };
+      })(),
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        },
+      },
+    clearScreen: false,
+  };
+});

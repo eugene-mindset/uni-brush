cab181e6d695a46f5d4f07296063761fee559bb0
Eugene Asare <eugenemindset@gmail.com>
2025-10-29 22:41:57 -0400

    Cleanup hook for feature

--- src/features/procedural-galaxy-creator/index.tsx
+++ src/features/procedural-galaxy-creator/index.tsx
@@ -1,53 +1,14 @@
-import { FunctionalComponent, JSX } from "preact";
-import { useRef } from "preact/hooks";
+import { FunctionalComponent } from "preact";
 
 import { Panel } from "@/components";
 
 import * as CreatorView from "./components";
-import { Creator, EntityTypes } from "@/models";
-import { Entity } from "@/models";
-import { Vector3 } from "three";
+import { EntityTypes } from "@/models";
 
-const initModel = () => {
-  const model = new Creator.Base.EntityFactory<Entity.StarSystem.EntityType>();
-  const mainEntityPipe = model.createPipeline(1750, "Primary Shape");
-  mainEntityPipe.createPipeline("initialPosition");
-  mainEntityPipe.createPipeline("name");
-
-  mainEntityPipe.pipelines["initialPosition"]?.setGenerator(Creator.Generators.NormalDistribution);
-  mainEntityPipe.pipelines["initialPosition"]?.createOperator(Creator.Operators.BasicGravity);
-  mainEntityPipe.pipelines["initialPosition"]?.createOperator(Creator.Operators.ArmGravity);
-
-  mainEntityPipe.pipelines["name"]?.setGenerator(Creator.Generators.DefaultValue<string>);
-  mainEntityPipe.pipelines["name"]?.generator?.setConfig({ defaultValue: "Primary Star" });
-
-  const testEntityPipe = model.createPipeline(750, "Secondary Shape");
-  testEntityPipe.createPipeline("initialPosition");
-  testEntityPipe.createPipeline("name");
-
-  testEntityPipe.pipelines["initialPosition"]?.setGenerator(Creator.Generators.NormalDistribution);
-
-  testEntityPipe.pipelines["name"]?.setGenerator(Creator.Generators.DefaultValue<string>);
-  testEntityPipe.pipelines["name"]?.generator?.setConfig({ defaultValue: "Secondary Star" });
-
-  return model;
-};
+import { useProceduralCreatorModel } from "./hooks";
 
 export const ProceduralCreator: FunctionalComponent<{}> = () => {
-  const coreModelRef = useRef(initModel());
-
-  const onClickGenerate = (_event: JSX.TargetedMouseEvent<HTMLButtonElement>) => {
-    coreModelRef.current.generate();
-    const outputs = coreModelRef.current.getOutputs();
-    console.log(coreModelRef);
-
-    Entity.StarSystem.Manager.fullReset();
-    Entity.StarSystem.Manager.batchInitializeProperty(
-      "initPos",
-      outputs.initialPosition as Vector3[]
-    );
-    Entity.StarSystem.Manager.batchInitializeProperty("name", outputs.name as string[]);
-  };
+  const { coreModel, generate } = useProceduralCreatorModel();
 
   return (
     <Panel title="Geography / Editor" canToggle width="650px" maxHeight="1000px">
@@ -65,13 +26,10 @@ export const ProceduralCreator: FunctionalComponent<{}> = () => {
           <Panel.Input type="checkbox" labelText="Lanes" />
         </div>
       </div>
-      <CreatorView.BaseFactoryComponent
-        factory={coreModelRef.current}
-        entity={EntityTypes.STAR_SYSTEM}
-      />
+      <CreatorView.BaseFactoryComponent factory={coreModel} entity={EntityTypes.STAR_SYSTEM} />
       <div className="space-top gap flex-row justify-right">
         <button className="core float-right">Save</button>
-        <button className="core float-right" onClick={onClickGenerate}>
+        <button className="core float-right" onClick={generate}>
           Generate
         </button>
       </div>
